eaf7dad8f94948b0c0e3bb5b134e27d34440fe72
Bug Williams <zachwilliams.services@gmail.com>
2021-04-06 15:14:37 -0400

    Initial Commit

---
+++ Blocky Calendar/Blocky_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blocky_CalendarApp.swift
+//  Blocky Calendar
+//
+//  Created by Bug on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Blocky_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blocky Calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blocky Calendar
+//
+//  Created by Bug on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
